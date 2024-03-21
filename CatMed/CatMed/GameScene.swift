c905f9c74452f498367521c92bc55e912aa585dc
elohMachado <heloisa.per.machado@gmail.com>
2024-03-21 16:16:23 -0300

    Implementei o score

--- CatMed/CatMed/GameScene.swift
+++ CatMed/CatMed/GameScene.swift
@@ -13,25 +13,40 @@ class GameScene: SKScene {
     var mouth: SKSpriteNode!
     var med: SKSpriteNode!
     
+    var scoreLabel: SKLabelNode!
+    
+    var score: Int = 0 {
+            didSet {
+                // Atualizar o texto da label de pontuação sempre que o valor da pontuação for alterado
+                scoreLabel.text = "Score: \(score)"
+            }
+        }
+    
     var isMouthOpen = false
     
     override func didMove(to view: SKView) {
         mouth = childNode(withName: "mouth") as? SKSpriteNode
         med = childNode(withName: "med") as? SKSpriteNode
+        scoreLabel = childNode(withName: "scoreLabel") as? SKLabelNode
+        
         
         med.userData = ["initialPosition": med.position]
+        
         
         self.mouth.isHidden = true
         
         openMouth()
         
+        //        physicsWorld.contactDelegate = self
     }
     
     func touchDown(atPoint pos : CGPoint) {
-        
+        moveMedTowardsMouth()
         
         if isMouthOpen {
-            moveMedTowardsMouth()
+            increaseScore(by: 1)
+        } else if !isMouthOpen {
+            score = 0
         }
         
         
@@ -63,39 +78,28 @@ class GameScene: SKScene {
     
     
     override func update(_ currentTime: TimeInterval) {
-    
+        if med.intersects(mouth) {
+            if let initialMedPosition = med.userData?["initialPosition"] as? CGPoint {
+                med.position = initialMedPosition
+            }
+        }
     }
     
     func moveMedTowardsMouth() {
-        let mouthPosition = mouth.position
-        
-        let dx = mouthPosition.x - med.position.x
-        let dy = mouthPosition.y - med.position.y
-        
-        let distance = sqrt(dx * dx + dy * dy)
-        
-        let desiredSpeed: CGFloat = 700.0
-        
-        let duration = TimeInterval(distance / desiredSpeed)
-        
-        let moveAction = SKAction.move(to: mouthPosition, duration: duration)
-        
-        let collisionAction = SKAction.run {
+            let mouthPosition = mouth.position
+            let dx = mouthPosition.x - med.position.x
+            let dy = mouthPosition.y - med.position.y
+            let distance = sqrt(dx * dx + dy * dy)
+            let desiredSpeed: CGFloat = 700.0
+            let duration = TimeInterval(distance / desiredSpeed)
+            let moveAction = SKAction.move(to: mouthPosition, duration: duration)
+            let sequence = SKAction.sequence([moveAction])
+            med.run(sequence)
             
-            if self.med.intersects(self.mouth) {
-                
-                print("Med collided with the mouth!")
-                self.med.isHidden = true
-                
-            }
+            // Aumentar a pontuação quando o jogador acerta a boca
+            
         }
-        
-        // Create a sequence of actions: move towards mouth, then check for collision
-        let sequence = SKAction.sequence([moveAction, collisionAction])
-        
-        // Run the sequence on the med node, and repeat forever
-        med.run(SKAction.repeatForever(sequence))
-    }
+    
     
     func openMouth() {
         let randomDuration = TimeInterval.random(in: 0.4...2.0)
@@ -129,6 +133,24 @@ class GameScene: SKScene {
         
         mouth.run(sequence)
     }
+    
+    func didBegin(_ contact: SKPhysicsContact) {
+        // Check if the contact is between med and mouth
+        if (contact.bodyA.node == med && contact.bodyB.node == mouth) ||
+            (contact.bodyA.node == mouth && contact.bodyB.node == med) {
+            // Reset med's position to its initial position
+            if let initialMedPosition = med.userData?["initialPosition"] as? CGPoint {
+                med.position = initialMedPosition
+            }
+            // Remove med's movement actions
+            med.removeAllActions()
+        }
+    }
+    
+    func increaseScore(by amount: Int) {
+            // Aumentar a pontuação
+            score += amount
+        }
     
 }
 
